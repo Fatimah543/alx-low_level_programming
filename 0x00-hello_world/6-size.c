b7eb3f7c07a8590cad80aef1aab6053aa59c39bf
Fatimah543 <bidemimama7@gmail.com>
2022-04-07 15:41:08 -0700

    l

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,14 @@
 #!/bin/bash
-printf("size of int: %zu bytes/n", sizeof(intType)); return 0;
+#include<stdio.h>
+int main() {
+	int intType;
+	float floatType;
+	double doubleType;
+
+	// sizeof evaluates the size of a variable
+	printf("size of int: %zu bits\n", sizeof(intType));
+	printf("size of float: %zu bits\n", sizeof(floatType));
+	printf("size of double: %zu bits\n", sizeof(doubleType));
+
+	return 0;
+}
