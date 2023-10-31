e6f212694a2c08c5a792cf7c6f07f73bd393c043
Fatimah-x0x0 <abdullahifatimah203@gmail.com>
2023-10-31 05:39:44 -0700

    low-level-program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main - main block
+ * return: 0
+ */
+
+int main(void)
+{
+  puts("\"programming is like building a multilingual puzzle");
+  return (0);
+
+}
