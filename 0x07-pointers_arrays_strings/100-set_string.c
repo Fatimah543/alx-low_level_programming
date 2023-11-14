f8dfd5d46bd14724debbab06d45e1ed2b6c716c4
Fatimah-x0x0 <abdullahifatimah203@gmail.com>
2023-11-14 11:08:49 -0800

    even pointers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
+ *
+ * Created by: Abdullahi Fatimah
+ * cc: 14th, november 2023.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
